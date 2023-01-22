aa1d0131ed3452fa6135a5beb9fa421c1934f49b
atreidas1 <atreidas!@gmail.com>
2023-01-22 23:40:23 +0300

    Add ardupilot modes

---
+++ Inc/ardupilot_modes.h
@@ -0,0 +1,29 @@
+#ifndef ARDUPILOT_MODES_H_
+#define ARDUPILOT_MODES_H_
+/*
+ * ArduPilotmodes
+ */
+uint8_t* defineCustomMode(uint32_t code) {
+	switch (code) {
+		case 0: return "MANUAL";
+		case 1: return "CIRCLE";
+		case 2: return "STABILIZE";
+		case 3: return "TRAINING";
+		case 4: return "ACRO";
+		case 5: return "FBWA";
+		case 6: return "FBWB";
+		case 7: return "CRUISE";
+		case 8: return "AUTOTUNE";
+		case 10: return "AUTO";
+		case 11: return "RTL";
+		case 12: return "LOITER";
+		case 13: return "TAKEOFF";
+		case 14: return "AVOID_ADSB";
+		case 15: return "GUIDED";
+		case 16: return "INITIALISING";
+		case 24: return "THERMAL";
+		default:
+			return "UNKNOWN";
+	}
+}
+#endif /* ARDUPILOT_MODES_H_ */
